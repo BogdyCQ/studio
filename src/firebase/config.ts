cbf139ad496d951f4f933bbfc69d42c2afbfa771
Bogdan Ciora <bogdan.ciora@qubiz.com>
2025-11-08 08:59:53 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
   "projectId": "studio-6454761164-25ccf",
   "appId": "1:217811837303:web:1d77d2ed813198f01bc172",
-  "storageBucket": "studio-6454761164-25ccf.appspot.com",
   "apiKey": "AIzaSyCVdiVIK6O5schZTUPfp4izdQAW3j9tvvw",
   "authDomain": "studio-6454761164-25ccf.firebaseapp.com",
+  "measurementId": "",
   "messagingSenderId": "217811837303"
 };
